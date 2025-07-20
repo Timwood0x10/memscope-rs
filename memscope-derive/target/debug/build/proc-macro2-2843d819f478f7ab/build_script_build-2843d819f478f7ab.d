48e22ac40d4663d263e21ccc9fbabe535c743c23
Timwood0x10 <121157680+Timwood0x10@users.noreply.github.com>
2025-07-20 14:58:05 +0800

    fix commit

--- memscope-derive/target/debug/build/proc-macro2-2843d819f478f7ab/build_script_build-2843d819f478f7ab.d
+++
@@ -1,5 +0,0 @@
-/Users/scc/code/rustcode/trace_tools/memscope-derive/target/debug/build/proc-macro2-2843d819f478f7ab/build_script_build-2843d819f478f7ab.d: /Users/scc/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/proc-macro2-1.0.95/build.rs
-
-/Users/scc/code/rustcode/trace_tools/memscope-derive/target/debug/build/proc-macro2-2843d819f478f7ab/build_script_build-2843d819f478f7ab: /Users/scc/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/proc-macro2-1.0.95/build.rs
-
-/Users/scc/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/proc-macro2-1.0.95/build.rs:
--- memscope-derive/target/debug/deps/unicode_ident-bfee75bf9bf65781.d
+++
@@ -1,6 +0,0 @@
-/Users/scc/code/rustcode/trace_tools/memscope-derive/target/debug/deps/unicode_ident-bfee75bf9bf65781.d: /Users/scc/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.18/src/lib.rs /Users/scc/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.18/src/tables.rs
-
-/Users/scc/code/rustcode/trace_tools/memscope-derive/target/debug/deps/libunicode_ident-bfee75bf9bf65781.rmeta: /Users/scc/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.18/src/lib.rs /Users/scc/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.18/src/tables.rs
-
-/Users/scc/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.18/src/lib.rs:
-/Users/scc/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.18/src/tables.rs:
